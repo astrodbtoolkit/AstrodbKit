96bb8e33d04c9ee2beffefefd0a811d3ff98664a
David Rodriguez <drodriguez@stsci.edu>
2020-07-10 13:55:48 -0400

    adding astroquery Simbad function and expanding tests

--- astrodbkit2/utils.py
+++ astrodbkit2/utils.py
@@ -1,8 +1,11 @@
+# Utility functions for Astrodbkit2
 
+import re
 from datetime import datetime
 from decimal import Decimal
+from astroquery.simbad import Simbad
 
-__all__ = ['json_serializer']
+__all__ = ['json_serializer', 'get_simbad_names']
 
 
 def json_serializer(obj):
@@ -18,3 +21,52 @@ def json_serializer(obj):
 
     return obj.__dict__
 
+
+def _name_formatter(name):
+    """
+    Clean up names of spurious formatting (extra spaces, some special characters)
+
+    Parameters
+    ----------
+    name : str
+        Name to clean up
+
+    Returns
+    -------
+    Cleaned up name
+    """
+
+    # Clean up multiple spaces
+    name = re.sub("\s\s+", " ", name)
+
+    # Clean up Simbad types
+    strings_to_delete = ['V* ', 'EM* ', 'NAME ', '** ', 'Cl* ', '* ']
+    for pattern in strings_to_delete:
+        name = name.replace(pattern, '')
+
+    name = name.strip()
+
+    # Clean up 'hidden' names from Simbad
+    if 'HIDDEN' in name.upper():
+        name = None
+
+    return name
+
+
+def get_simbad_names(name):
+    """
+    Get list of alternate names from Simbad
+
+    Parameters
+    ----------
+    name : str
+        Name to resolve
+
+    Returns
+    -------
+    List of names
+    """
+
+    t = Simbad.query_objectids(name)
+    temp = [_name_formatter(s) for s in t['ID'].tolist()]
+    return [s for s in temp if s is not None and s != '']
