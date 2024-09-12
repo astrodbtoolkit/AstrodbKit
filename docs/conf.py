6dfe0a51254ddba8591d3fa385c208d2f31a0020
David Rodriguez <drodriguez@stsci.edu>
2024-09-12 10:05:35 -0400

    Updating logo text

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,7 @@ release = package.__version__
 
 
 html_theme_options = {
-    'logotext1': 'AstrodbKit2',  # white,  semi-bold
+    'logotext1': 'AstrodbKit',  # white,  semi-bold
     'logotext2': '',  # orange, light
     'logotext3': ':docs'   # white,  light
     }
